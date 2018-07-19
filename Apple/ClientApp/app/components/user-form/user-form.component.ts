8f31e984104d5e41d029c2053ef94391989475c2
Martin Kozma <martin@kozmatech.com>
2018-07-19 13:42:46 +1000

    Add a user form

---
+++ Apple/ClientApp/app/components/user-form/user-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'user-form',
+    templateUrl: 'user-form.component.html'
+})
+
+export class UserFormComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
