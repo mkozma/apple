9a74122399f53906ee92d5f60e0601c4685bc267
Martin Kozma <martin@kozmatech.com>
2018-07-19 13:46:19 +1000

    user form

--- Apple/ClientApp/app/app.shared.module.ts
+++ Apple/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        UserFormComponent
     ],
     imports: [
         CommonModule,
@@ -27,6 +29,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'user', component: UserFormComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
